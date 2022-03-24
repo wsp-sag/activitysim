611f45a9bd4aff0039b9605358eb0fa37301f731
Sijia Wang <wangsijia0628@gmail.com>
2022-03-24 15:54:53 -0400

    change debug image entrypoint to test auto ownership

--- docker/entrypoint_debug.sh
+++ docker/entrypoint_debug.sh
@@ -3,4 +3,4 @@
 # run tests in live debug
 source ../../opt/conda/etc/profile.d/conda.sh
 conda activate ASIM-DEV
-python -m debugpy --listen 0.0.0.0:5678 --wait-for-client ../../opt/conda/envs/ASIM-DEV/bin/pytest activitysim/abm/test/test_misc/test_summarize.py
+python -m debugpy --listen 0.0.0.0:5678 --wait-for-client ../../opt/conda/envs/ASIM-DEV/bin/pytest activitysim/abm/test/test_misc/test_auto_ownership.py
