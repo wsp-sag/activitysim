905d995858fa20bc19be2dd35f83a9a416318823
Sijia Wang <wangsijia0628@gmail.com>
2022-03-24 19:00:41 -0400

    add accessibility to test auto ownership

--- activitysim/abm/test/test_misc/test_auto_ownership.py
+++ activitysim/abm/test/test_misc/test_auto_ownership.py
@@ -8,10 +8,11 @@ from activitysim.abm import models
 from activitysim.core import pipeline, config
 from activitysim.core import tracing
 
+logger = logging.getLogger(__name__)
 
 # Used by conftest.py initialize_pipeline method
 @pytest.fixture(scope='module')
-def module() -> str:
+def module(prepare_module_inputs) -> str:
     """
     A pytest fixture that returns the data folder location.
     :return: folder location for any necessary data to initialize the tests
@@ -31,7 +32,7 @@ def tables() -> dict[str, str]:
         'land_use': 'zone_id',
         'persons': 'person_id',
         'households': 'household_id',
-        #'accessibility': 'mgra'
+        'accessibility': 'zone_id'
     }
 
 
@@ -61,18 +62,15 @@ def test_auto_ownership(initialize_pipeline: pipeline.Pipeline, caplog):
 
     # compare with targets
     # TODO check the value of auto_ownership
-    """
-    target_df = pd.DataFrame()
-    model_df = household_df['auto_ownership'].value_counts().to_frame()
-    assert (target_df == model_df)
-    """
+    if validate_model_against_target(prepare_targets, household_df):
+        logger.info("Model result matches target")
+    else:
+        logger.info("Model result does not match target")
 
-## TODO
 # fetch/prepare existing files for model inputs
 # e.g. read accessibilities.csv from ctramp result, rename columns, write out to accessibility.csv which is the input to activitysim
-
 @pytest.fixture(scope='module')
-def prepare_model_inputs() -> None:
+def prepare_module_inputs() -> None:
     """
     Prepare accessibility file from ctramp run into input file activitysim expects
     1. renaming columns
@@ -84,3 +82,95 @@ def prepare_model_inputs() -> None:
 
     :return: None
     """
+
+    # this is the ctramp accessibility output file
+    # can be downloaded from 
+    # https://wsponlinenam.sharepoint.com/:x:/r/sites/US-TM2ConversionProject/Shared%20Documents/Task%203%20ActivitySim/model_results/
+    # ver12_new_inputs/ctramp_output/accessibilities.csv?d=wef3dbcd2186c42518a9e9558a15a2ca3&csf=1&web=1&e=6whUv0
+    input_accessibility_file = os.path.join('test', 'auto_ownership', 'data', 'accessibilities.csv')
+
+    accessibility_df = pd.read_csv(
+        input_accessibility_file
+    )
+
+    # rename columns
+    # this dictionary is developed based on mtc ctramp code
+    # https://github.com/BayAreaMetro/travel-model-two/blob/27e3ad6e5a6c71120e3e513fe423ecaac372c63a/core/src/java/com/pb/mtctm2/abm/accessibilities/AccessibilitiesTable.java#L24-L61
+    # TODO confirm this is the corrent column names
+    accessibility_columns_dict = {
+        'column_1': 'nonmandatory_auto_accessibility',
+        'column_2': 'nonmandatory_transit_accessibility',
+        'column_3': 'nonmandatory_nm_accessibility',
+        'column_4': 'nonmandatory_sov0_accessibility',
+        'column_5': 'nonmandatory_sov1_accessibility',
+        'column_6': 'nonmandatory_sov2_accessibility',
+        'column_7': 'nonmandatory_hov0_accessibility',
+        'column_8': 'nonmandatory_hov1_accessibility',
+        'column_9': 'nonmandatory_hov2_accessibility',
+        'column_10': 'shop_hov_insufficient_accessibility',
+        'column_11': 'shop_hov_sufficient_accessibility',
+        'column_12': 'shop_hov_oversufficient_accessibility',
+        'column_13': 'maint_hov_insufficient_accessibility',
+        'column_14': 'maint_hov_sufficient_accessibility',
+        'column_15': 'maint_hov_oversufficient_accessibility',
+        'column_16': 'eat_hov_insufficient_accessibility',
+        'column_17': 'eat_hov_sufficient_accessibility',
+        'column_18': 'eat_hov_oversufficient_accessibility',
+        'column_19': 'visit_hov_insufficient_accessibility',
+        'column_20': 'visit_hov_sufficient_accessibility',
+        'column_21': 'visit_hov_oversufficient_accessibility',
+        'column_22': 'discr_hov_insufficient_accessibility',
+        'column_23': 'discr_hov_sufficient_accessibility',
+        'column_24': 'discr_hov_oversufficient_accessibility',
+        'column_25': 'escort_hov_insufficient_accessibility',
+        'column_26': 'escort_hov_sufficient_accessibility',
+        'column_27': 'escort_hov_oversufficient_accessibility',
+        'column_28': 'shop_sov_insufficient_accessibility',
+        'column_29': 'shop_sov_sufficient_accessibility',
+        'column_30': 'shop_sov_oversufficient_accessibility',
+        'column_31': 'maint_sov_insufficient_accessibility',
+        'column_32': 'maint_sov_sufficient_accessibility',
+        'column_33': 'maint_sov_oversufficient_accessibility',
+        'column_40': 'discr_sov_insufficient_accessibility',
+        'column_41': 'discr_sov_sufficient_accessibility',
+        'column_42': 'discr_sov_oversufficient_accessibility',
+        'column_45': 'total_emp_accessibility',
+        'column_47': 'hh_walktransit_accessibility',
+        'mgra' : 'zone_id'
+    }
+
+    accessibility_df.rename(columns=accessibility_columns_dict, inplace = True)
+
+    accessibility_df.to_csv(
+        os.path.join('test', 'auto_ownership', 'data', 'accessibility.csv'),
+        index = False
+    )
+
+    ## TODO
+    # annotate household, person, and land_use data
+
+# TODO 
+# create target database from existing run
+@pytest.fixture(scope='module')
+def prepare_targets() -> pd.DataFrame:
+    """
+    Prepare auto ownership target data from existing ctramp run
+
+    :return: pd.DataFrame
+    """
+
+    # ctramp run result
+    # https://wsponlinenam.sharepoint.com/:x:/r/sites/US-TM2ConversionProject/Shared%20Documents/Task%203%20ActivitySim/model_results/
+    # ver12_new_inputs/ctramp_output/aoResults.csv?d=wba2db5a7cd8841ae822cc4234038b258&csf=1&web=1&e=KmHeWN
+
+# TODO
+# flesh out assert function
+def validate_model_against_target(target_df: pd.DataFrame, model_df: pd.DataFrame) -> bool:
+    """
+    assert funtion that compares model summary with target
+    e.g. loop through each cell in the summary table, if % diff within threshold then model matches target
+
+    :return: bool
+    """
+
+    return True
