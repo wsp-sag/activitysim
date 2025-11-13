f9fb521f484b226324a536ddbcbfd23d7ab3d966
i-am-sijia <i-am-sijia@users.noreply.github.com>
2025-11-13 14:36:44 +0000

    deploy: 5963d038c5c05798ef061ea9ad469a62326e363c

--- develop/_static/documentation_options.js
+++ develop/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.5.dev1893+ged44a4061',
+    VERSION: '1.0.5.dev1897+g5963d038c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
