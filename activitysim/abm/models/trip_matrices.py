78d82979ac204f5ed341c294ec91af9734e2a20e
Sijia Wang <wangsijia0628@gmail.com>
2026-03-19 18:40:31 -0400

    Add global option to skip households on simulation failure (#1023)
    * skip hh in simple simulate
    
    * retain `household_id` by default in the drop_unused_columns()
    
    we need it for debug tracing and skipping failed hh
    
    * skip choice for interaction sample simulate
    
    * don't log hh id when bad choice df is empy
    
    * add hh id to user specified `chooser_columns` when `chooser_columns` exist
    
    * blacken
    
    * update test utils to probs raises
    
    * [skip ci] shadow price calc without failed choices
    
    * [skip ci] special case in trip mode choice
    
    * [skip ci] adjust sample_rate
    
    * [skip ci] add final logging of num hh skipped
    
    * unit test
    
    * update CI
    
    * track and summarize failed households by component
    
    * `skipped_household_ids` is now a dictionary
    
    * print more loggings about trips missing modes
    
    * adjust trip expansion factor using hh weight instead of count
    
    * update state tables only when new households are skipped
    
    * put a limit on num of households allowed to skip
    
    * auto debug tracing newly failed choice
    
    * update test
    
    * Add documentation to user guide
    
    * update doc
    
    * increase sample rate decimals
    
    * handle zero `sample_rate` when calculating weights
    
    * black
    
    * [skip ci] confirm trace files are generated for skipped hh
    
    * Revert "increase sample rate decimals"
    
    This reverts commit a734c16eb5beb1ddbe88bb575c02111c34442f50.
    
    * add note for not rounding sample rate
    
    * write trip matrices even when `sample_rate` is all zero
    
    To pass this single hh test: https://github.com/ActivitySim/activitysim/actions/runs/23202170408/job/67427319813?pr=1023

--- activitysim/abm/models/trip_matrices.py
+++ activitysim/abm/models/trip_matrices.py
@@ -90,6 +90,70 @@ def write_trip_matrices(
 
     trips_df = annotate_trips(state, trips, network_los, model_settings)
 
+    # This block adjusts household sample rate column to account for skipped households.
+    # Note: the `HH_EXPANSION_WEIGHT_COL` is pointing to the `sample_rate` column in the households table.
+    # Based on the calculation in write_matrices() function, the sample_rate is used to calculate the expansion weight as 1 / sample_rate.
+    # A sample_rate of 0.01 means the sample household should be expanded 1/0.01 = 100 times in the actual population households.
+    # In simulation, the `sample_rate` is calculated and added to the synthetic households
+    # based on household_sample_size / total_household_count, and therefore is the same for all households.
+    # In estimation, the `sample_rate` may vary by household, but weights are not used in estimation, and write_trip_matrices is not called during estimation.
+    # But we still try to cover both cases (when rates are the same vs when they vary) here for consistency.
+    hh_weight_col = model_settings.HH_EXPANSION_WEIGHT_COL
+    household_sample_rate = state.get_dataframe("households")[hh_weight_col]
+    zero_household_sample_rate = household_sample_rate == 0
+
+    # Check if there are households with zero `sample_rate`, which will result in inf expansion weight.
+    # If all households have zero `sample_rate`, then all trips will have inf expansion weight, we will use household count instead of weight for adjustment factor calculation
+    # If some households have zero `sample_rate`, we will print a warning and skip trips from those households in the trip matrix generation.
+    if zero_household_sample_rate.all():
+        logger.warning(
+            f"All households have {hh_weight_col} of 0, which will result in inf expansion weight. "
+            f"Trip matrix generation will be based on household count instead of weight."
+        )
+    elif zero_household_sample_rate.any():
+        logger.warning(
+            f"Some households have {hh_weight_col} of 0, which will result in inf expansion weight. "
+            f"Trips from those households will be skipped in trip matrix generation."
+        )
+        trips_df = trips_df[trips_df[hh_weight_col] != 0]
+        # skip trip matrix generation if there are no trips left after filtering out zero sample_rate households
+        if trips_df.empty:
+            logger.warning(
+                f"All trips have {hh_weight_col} of 0, which will result in inf expansion weight. "
+                f"Trip matrix generation will be skipped."
+            )
+            return
+
+    if state.get("num_skipped_households", 0) > 0:
+        logger.info(
+            f"Adjusting household sample rate in {hh_weight_col} to account for {state.get('num_skipped_households', 0)} skipped households."
+        )
+        skipped_sample_rate = state.get_dataframe("households_skipped")[hh_weight_col]
+        remaining_sample_rate = household_sample_rate
+
+        if zero_household_sample_rate.all():
+            # if all households have zero sample rate, we will use household count instead of weight for adjustment factor calculation
+            skipped_household_count = state.get("num_skipped_households", 0)
+            remaining_household_count = len(state.get_dataframe("households"))
+            total_household_count = skipped_household_count + remaining_household_count
+            adjustment_factor = remaining_household_count / total_household_count
+        else:
+            # adjust the hh sample rates to account for skipped households
+            # first get the total expansion weight of the skipped households, which will be the sum of inverse of their sample rates
+            skipped_household_weights = (
+                1 / skipped_sample_rate[skipped_sample_rate != 0]
+            ).sum()
+            # next get the total expansion weight of the remaining households
+            remaining_household_weights = (
+                1 / remaining_sample_rate[remaining_sample_rate != 0]
+            ).sum()
+            # the adjustment factor is the remaining household weight / (remaining household weight + skipped household weight)
+            total_household_weights = (
+                remaining_household_weights + skipped_household_weights
+            )
+            adjustment_factor = remaining_household_weights / total_household_weights
+        trips_df[hh_weight_col] = trips_df[hh_weight_col] * adjustment_factor
+
     if model_settings.SAVE_TRIPS_TABLE:
         state.add_table("trips", trips_df)
 
